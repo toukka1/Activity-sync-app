07a394d37c8bbe8c99ecef6ccc93556c4b00ce18
Touko Nurminen <touko.nurminen@aalto.fi>
2024-06-14 23:32:28 +0200

    switch to typescript

--- App.tsx
+++ App.tsx
@@ -5,23 +5,20 @@ import * as DocumentPicker from 'expo-document-picker'
 import * as FileSystem from 'expo-file-system'
 
 
-
 export default function App() {
-  const [fileUri, setFileUri] = useState('')
-
   const pickActivity = async () => {
-    const result = await DocumentPicker.getDocumentAsync({
+    const result: DocumentPicker.DocumentPickerResult = await DocumentPicker.getDocumentAsync({
       type: 'text/plain',
     })
 
-    setFileUri(result.assets[0].uri)
-    readFile()
-    createAndWriteToFile()
+    const uri: string = result.assets[0].uri
+    await readFile(uri)
+    await createAndWriteToFile()
   }
 
-  const readFile = async () => {
+  const readFile = async (uri: string) => {
     try {
-      const content = await FileSystem.readAsStringAsync(fileUri)
+      const content: string = await FileSystem.readAsStringAsync(uri)
       console.log(content)
     } catch (error) {
       console.error('Error reading file:', error)
@@ -30,15 +27,14 @@ export default function App() {
 
   const createAndWriteToFile = async () => {
     try {
-      const uri = `${FileSystem.documentDirectory}test.txt`
-      setFileUri(uri)
-      const content = 'Hello, this is some text content!'
+      const uri: string = `${FileSystem.documentDirectory}test.txt`
+      const content: string = 'Hello, this is some text content!'
 
-      await FileSystem.writeAsStringAsync(fileUri, content)
+      await FileSystem.writeAsStringAsync(uri, content)
 
       console.log('File written successfully!')
       console.log('reading file...')
-      readFile()
+      readFile(uri)
     } catch (error) {
       console.error('Error writing file:', error)
     }
