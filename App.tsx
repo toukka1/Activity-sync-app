20655bfedc4badef37640c6033d7d04cbbe2cb53
Touko Nurminen <touko.nurminen@aalto.fi>
2024-06-22 17:32:47 +0200

    converter working

--- App.tsx
+++ App.tsx
@@ -3,38 +3,53 @@ import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View, Button } from 'react-native'
 import * as DocumentPicker from 'expo-document-picker'
 import * as FileSystem from 'expo-file-system'
+import { FileData } from './types/fileTypes'
+import { convertOPHealthFileToGPX } from './utils/gpxUtils'
 
 
 export default function App() {
-  const pickActivity = async () => {
+  async function pickActivity () {
     const result: DocumentPicker.DocumentPickerResult = await DocumentPicker.getDocumentAsync({
       type: 'text/plain',
     })
 
-    const uri: string = result.assets[0].uri
-    await readFile(uri)
-    await createAndWriteToFile()
+    if (!result.canceled) {
+        const uri: string = result.assets[0].uri
+        console.log(uri)
+        console.log(result)
+        const file: string | undefined = await readFile(uri)
+        if (file) {
+          const fileJson: FileData = JSON.parse(file)
+          const gpx: string = await convertOPHealthFileToGPX(fileJson)
+          createAndWriteToFile(gpx)
+          const uri: string = `${FileSystem.documentDirectory}testgpx.gpx`
+          await readFile(uri)
+          console.log(uri)
+        }
+      }
+    else {
+      console.log('cancelled')
+    }
   }
 
-  const readFile = async (uri: string) => {
+  async function readFile (uri: string): Promise<string | undefined> {
     try {
       const content: string = await FileSystem.readAsStringAsync(uri)
       console.log(content)
+      return content
     } catch (error) {
       console.error('Error reading file:', error)
     }
   }
 
-  const createAndWriteToFile = async () => {
+  async function createAndWriteToFile (content: string) {
     try {
-      const uri: string = `${FileSystem.documentDirectory}test.txt`
-      const content: string = 'Hello, this is some text content!'
+      const uri: string = `${FileSystem.documentDirectory}testgpx.gpx`
 
       await FileSystem.writeAsStringAsync(uri, content)
 
       console.log('File written successfully!')
-      console.log('reading file...')
-      readFile(uri)
+
     } catch (error) {
       console.error('Error writing file:', error)
     }
