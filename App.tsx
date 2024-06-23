1428e04621fed703d77ec2f16766cd4796d4cb9e
Touko Nurminen <touko.nurminen@aalto.fi>
2024-06-24 00:09:36 +0200

    auth test

--- App.tsx
+++ App.tsx
@@ -1,17 +1,53 @@
-import { StyleSheet, View, StatusBar } from 'react-native'
+import { StyleSheet, View, StatusBar, Button, Alert } from 'react-native'
 import HomeScreen from './screens/HomeScreen'
 import Banner from './components/Banner'
 const logger = require('./utils/logger')
+import React, { useEffect, useState } from 'react'
+import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios from 'axios'
 
+
+const discovery = {
+  authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
+  tokenEndpoint: 'https://www.strava.com/oauth/token',
+  revocationEndpoint: 'https://www.strava.com/oauth/deauthorize',
+}
 
 export default function App() {
+
+  const [request, response, promptAsync] = useAuthRequest(
+    {
+      clientId: 'CLIENT_ID',
+      scopes: ['activity:read_all'],
+      redirectUri: makeRedirectUri({
+        native: 'myapp://myapp.com',
+      }),
+    },
+    discovery
+  )
+
+  React.useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params
+    }
+  }, [response])
+
+  console.log(response)
 
   logger.info('moi')
   return (
     <View style={styles.container}>
       <StatusBar backgroundColor="#007AFF" barStyle="light-content"/>
-      <Banner />
-      <HomeScreen />
+      {/* <Banner /> */}
+      {/* <HomeScreen /> */}
+      <Button
+        disabled={!request}
+        title="Login"
+        onPress={() => {
+          promptAsync()
+        }}
+      />
     </View>
   )
 }
