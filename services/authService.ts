1d040488745dd8af85767ff2a42c7c98e3d6f385
Touko <touko.nurminen@gmail.com>
2024-10-22 13:59:00 +0300

    Notify user on successful/failed login/upload

--- services/authService.ts
+++ services/authService.ts
@@ -2,48 +2,51 @@ import { makeRedirectUri, useAuthRequest, AuthSessionResult } from 'expo-auth-se
 import * as SecureStore from 'expo-secure-store'
 import axios from 'axios'
 import { useEffect } from 'react'
+import { Alert } from 'react-native'
 
 const logger = require('../utils/logger')
 const config = require('../utils/config')
 
 const discovery = {
-  authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
-  tokenEndpoint: 'https://www.strava.com/oauth/token',
-  revocationEndpoint: 'https://www.strava.com/oauth/deauthorize',
+    authorizationEndpoint: 'https://www.strava.com/oauth/mobile/authorize',
+    tokenEndpoint: 'https://www.strava.com/oauth/token',
+    revocationEndpoint: 'https://www.strava.com/oauth/deauthorize',
 }
 
 export function useStravaAuthRequest() {
-  const [request, response, promptAsync] = useAuthRequest(
-    {
-      clientId: config.STRAVA_CLIENT_ID,
-      scopes: ['activity:write'],
-      redirectUri: makeRedirectUri(),
-    },
-    discovery
-  )
+    const [request, response, promptAsync] = useAuthRequest(
+        {
+            clientId: config.STRAVA_CLIENT_ID,
+            scopes: ['activity:write'],
+            redirectUri: makeRedirectUri(),
+        },
+        discovery
+    )
 
-  // Handle response and token exchange
-  useEffect(() => {
-    if (response?.type === 'success') {
-      const { code } = response.params
+    // Handle response and token exchange
+    useEffect(() => {
+        if (response?.type === 'success') {
+            const { code } = response.params
 
-      // Exchange authorization code for access token
-      axios.post('https://www.strava.com/oauth/token', {
-        client_id: config.STRAVA_CLIENT_ID,
-        client_secret: config.STRAVA_CLIENT_SECRET,
-        code,
-        grant_type: 'authorization_code',
-      })
-        .then(response => {
-          const accessToken = response.data.access_token
-          // Store the token for later use
-          SecureStore.setItemAsync('strava_access_token', accessToken)
-        })
-        .catch(error => {
-          logger.error('Error exchanging authorization code for token:', error)
-        })
-    }
-  }, [response])
+            // Exchange authorization code for access token
+            axios.post('https://www.strava.com/oauth/token', {
+                client_id: config.STRAVA_CLIENT_ID,
+                client_secret: config.STRAVA_CLIENT_SECRET,
+                code,
+                grant_type: 'authorization_code',
+            })
+                .then(response => {
+                    const accessToken = response.data.access_token
+                    // Store the token for later use
+                    SecureStore.setItemAsync('strava_access_token', accessToken)
+                    Alert.alert('Authentication Success', 'You are successfully authenticated with Strava!')
+                })
+                .catch(error => {
+                    logger.error('Error exchanging authorization code for token:', error)
+                    Alert.alert('Authentication Error', 'Failed to authenticate with Strava. Please try again.')
+                })
+        }
+    }, [response])
 
-  return { request, response, promptAsync }
+    return { request, response, promptAsync }
 }
