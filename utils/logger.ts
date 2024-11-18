995015f22e6fce97556f612f2725199a6edf277b
Touko <touko.nurminen@gmail.com>
2024-11-18 21:16:45 +0200

    reformat logger

--- utils/logger.ts
+++ utils/logger.ts
@@ -1,17 +1,17 @@
 import { LogParams } from '../types/types'
 
 const info = (...params: LogParams) => {
-  if (__DEV__) {
-    console.log(...params)
-  }
+    if (__DEV__) {
+        console.log(...params)
+    }
 }
 
 const error = (...params: LogParams) => {
-  if (__DEV__) {
-    console.error(...params)
-  }
+    if (__DEV__) {
+        console.error(...params)
+    }
 }
 
 module.exports = {
-  info, error
+    info, error
 }
